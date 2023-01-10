12ad47a185d8f857471b76fd895148f0c03e4ffd
stbere <taihmuhshuu@gmail.com>
2023-01-09 18:30:26 -0600

    Added Data Analysis Portion of the Module

--- EmployeeSQL/schema_table.sql
+++ EmployeeSQL/schema_table.sql
@@ -6,7 +6,9 @@
 -- DROP TABLE salaries;
 -- DROP TABLE titles;
 
--- 1.) departments.csv schema
+---- DATA ENGINEERING ----
+
+-- 1a.) departments.csv schema
 -- Create the tables for departments.csv
 CREATE TABLE departments(dept_no VARCHAR NOT NULL,
 						 dept_name VARCHAR NOT NULL,
@@ -22,7 +24,7 @@ SELECT * FROM departments;
 
 -----------------------------------------------------------
 
--- 2.) employees.csv schema
+-- 2a.) employees.csv schema
 -- Create the tables for employees.csv
 CREATE TABLE employees(emp_no INT NOT NULL,
 					   emp_title_id VARCHAR NOT NULL,
@@ -42,7 +44,7 @@ SELECT * FROM employees
 
 -----------------------------------------------------------
 
--- 3.) dept_emp schema
+-- 3a.) dept_emp schema
 -- Create the tables for dept_emp.csv
 CREATE TABLE dept_emp(emp_no INT NOT NULL,
 						 dept_no VARCHAR NOT NULL,
@@ -59,7 +61,7 @@ SELECT * FROM dept_emp;
 
 -----------------------------------------------------------
 
--- 4.) dept_manager schema
+-- 4a.) dept_manager schema
 -- Create the tables for dept_manager.csv
 CREATE TABLE dept_manager(dept_no VARCHAR NOT NULL,
 						 emp_no INT NOT NULL,
@@ -76,7 +78,7 @@ SELECT * FROM dept_manager;
 
 -----------------------------------------------------------
 
--- 5.) salaries schema
+-- 5a.) salaries schema
 -- Create the tables for salaries.csv
 CREATE TABLE salaries(emp_no INT NOT NULL,
 					  salary INT NOT NULL,
@@ -91,13 +93,79 @@ SELECT * FROM salaries
 
 -----------------------------------------------------------
 
--- 6.) titles schema
+-- 6a.) titles schema
 -- Create the tables for departments.csv
 CREATE TABLE titles(title_id VARCHAR NOT NULL,
 				   title VARCHAR NOT NULL);
+				   
 
 -- Import would not work due to 'columns' error, using 'copy' method instead
 COPY titles FROM 'c:/data/data/titles.csv' 
 WITH CSV HEADER;
 
 SELECT * FROM titles;
+
+-----------------------------------------------
+
+---- DATA MODELING ----
+
+-- Check repository for Quick Database Diagram: 
+-- https://github.com/stbere/sql-challenge/tree/main/EmployeeSQL/Entity_Relationship_Diagrams
+
+-----------------------------------------------
+
+---- DATA ANALYSIS ----
+
+-- 1b.) List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+-- 2b.) List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- 3b.) List the manager of each department along with their department number, 
+--      department name, employee number, last name, and first name.
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
+FROM dept_manager m
+JOIN departments d ON m.dept_no = d.dept_no
+JOIN employees e ON m.emp_no = e.emp_no;
+
+-- 4b.) List the department number for each employee along with that employee’s employee number, 
+--      last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_no, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+-- 5b.) List first name, last name, and sex of each employee whose first name is Hercules and whose 
+--      last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6b.) List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- 7b.) List each employee in the Sales and Development departments, including their employee number, last name, 
+--      first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- 8b.) List the frequency counts, in descending order, of all the employee last names 
+--     (that is, how many employees share each last name).
+SELECT last_name, COUNT(*) AS count
+FROM employees
+GROUP BY last_name
+ORDER BY count DESC;
+
+
